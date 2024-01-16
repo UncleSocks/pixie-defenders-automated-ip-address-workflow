651ad9fbc7e37bd0fee4c2f8b0f1e852c215b010
UncleSocks <ilisan.tyrone@gmail.com>
2024-01-16 09:18:18 +0800

    Update: Modified functions to match PEP8 format.

--- iplooker.py
+++ iplooker.py
@@ -3,66 +3,62 @@ import maskpass
 import argparse
 from collections import Counter
 
-
-accessToken = maskpass.askpass("Enter IPInfo Token: ")
-handler = ipinfo.getHandler(accessToken)
-orgKeyword = input("Enter organization keyword (e.g., Microsoft): ").lower()
-
-negCheck = orgKeyword.split(' ')
-print(negCheck)
-
-parser = argparse.ArgumentParser(description="IP address checker for Microsoft and Amazon addresses")
-parser.add_argument("-i","--ipList")
+parser = argparse.ArgumentParser(description="IP address checker using IPInfo API. Enter the organization keyword/s for searching, use space as a separator for multiple keywords; prepend the 'NOT' keyword to negate the search. The '-' character will process all IP addresses in the list without any keyword.")
+parser.add_argument("-i","--ipList",help="Specify the location of the text file containing the IP addresses to be processed.")
 args = parser.parse_args()
-
 ipList = []
 with open(args.ipList) as list:
     for ip in list:
         ip = ip.strip()
         ipList.append(ip)
 
-listOfList = []
-listcount = 0
+accessToken = maskpass.askpass("Enter token: ")
+handler = ipinfo.getHandler(accessToken)
+orgKeyword = input("Enter organization keyword (e.g., Microsoft): ").lower()
+keywordList = orgKeyword.split(' ')
 
-if negCheck[0] == "-":
+output = []
+keywordCount = 0
+
+if keywordList[0] == "-":
     for ip in ipList:
         details = handler.getDetails(ip)
         ipOutput = str(details.ip + "[" + details.country + ":" + details.org.split(' ',1)[1].upper() + "]")
-        listOfList.append(ipOutput)
-    listcount = listcount + 1        
-elif negCheck[0] == "not":
-    for check in negCheck[1:]:
+        output.append(ipOutput)
+    keywordCount = keywordCount + 1        
+elif keywordList[0] == "not":
+    for keyword in keywordList[1:]:
         for ip in ipList:
             details = handler.getDetails(ip)
             ipOrg = str(details.org.lower())
-            if check not in ipOrg:
+            if keyword not in ipOrg:
                 ipOutput = str(details.ip + "[" + details.country + ":" + details.org.split(' ',1)[1].upper() + "]")
-                listOfList.append(ipOutput)
-        listcount = listcount+1
-elif negCheck[0] != "not":
-    for check in negCheck[0:]:
+                output.append(ipOutput)
+        keywordCount = keywordCount + 1
+elif keywordList[0] != "not":
+    for keyword in keywordList[0:]:
         for ip in ipList:
             details = handler.getDetails(ip)
             ipOrg = str(details.org.lower())
-            if check in ipOrg:
+            if keyword in ipOrg:
                 ipOutput = str(details.ip + "[" + details.country + ":" + details.org.split(' ',1)[1].upper() + "]")
-                listOfList.append(ipOutput)
-        listcount = listcount+1
+                output.append(ipOutput)
+        keywordCount = keywordCount + 1
 else:
-    print("ERROR BOGO")
+    print("ERROR! Use the '-h' for information on how to use the tool.")
 
-if listcount == 1:
-    for entry in listOfList:
+
+if keywordCount == 1:
+    for entry in output:
         print(entry, end="\n")
-elif listcount > 1 and negCheck[0] == "not": 
-    counter = Counter(listOfList)
-    string_duplicates = [item for item, count in counter.items() if count > 1]
-    for entry in string_duplicates:
-        print(entry, end="/n")
-elif listcount > 1 and negCheck[0] != "not":
-    uniqueValue = set(listOfList)
-    print(uniqueValue)
+elif keywordCount > 1 and keywordList[0] == "not": 
+    counter = Counter(output)
+    duplicates = [item for item, count in counter.items() if count > (keywordCount - 1)]
+    for entry in duplicates:
+        print(entry, end="\n")
+elif keywordCount > 1 and keywordList[0] != "not":
+    uniques = set(output)
+    for entry in uniques:
+        print(entry)
 else:
-    print("ERROR!")
-
-41aba1d6582a8b
+    print("ERROR! Use the '-h' for information on how to use the tool.")
